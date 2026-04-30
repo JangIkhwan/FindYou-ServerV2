615d6d0bc4def1f05d849b7c4cc0603443ac25f7
JangIkhwan <bluensky0213@gmail.com>
2026-04-30 12:33:26 +0900

    feat : 구조동물신고글 스테이징 테이블 레포지토리 작성

---
+++ src/main/java/com/kuit/findyou/domain/report/repository/sync/PublicAnimalStagingRepository.java
@@ -0,0 +1,9 @@
+package com.kuit.findyou.domain.report.repository.sync;
+
+import com.kuit.findyou.domain.report.model.sync.PublicAnimalStaging;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublicAnimalStagingRepository extends JpaRepository<PublicAnimalStaging, Long> {
+}
